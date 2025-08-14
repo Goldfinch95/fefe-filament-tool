419886abc26bd8d973c365d04d0a54019b773896
Goldfinch <facu_vila@hotmail.com>
2025-08-14 11:20:52 -0300

    se elimino elementos del sidebar que no se usan

--- components/ui/AppSidebar.tsx
+++ components/ui/AppSidebar.tsx
@@ -3,25 +3,19 @@
 import * as React from "react"
 import {
   AudioWaveform,
-  BookOpen,
-  Bot,
+  
   Command,
-  Frame,
+
   GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
+  
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
+
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
+  
   SidebarHeader,
   SidebarRail,
   SidebarMenu,
