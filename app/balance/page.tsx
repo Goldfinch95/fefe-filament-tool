125e86a9e12c14b19f0e02691cd4e9db2534a4d7
Goldfinch <facu_vila@hotmail.com>
2025-09-12 10:58:32 -0300

    se agrego los bloques visuales de debe,haber y resultado de la pagina de balance

--- app/balance/page.tsx
+++ app/balance/page.tsx
@@ -1,11 +1,10 @@
+import { SectionCards } from "./section-cards";
+
 export default function BalancePage() {
   return (
-    <div className="min-h-screen w-screen bg-[url('/fefe.jfif')] bg-cover bg-center flex flex-1 flex-col gap-4 p-4 pt-0">
-      
-      <div id="balance" className="grid py-2 auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+    <div className="min-h-screen flex flex-1 flex-col gap-4 p-4 pt-18 sm:pt-0">
+      <div id="balance">
+        <SectionCards  />
       </div>
       <div id="ventas" className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
       <div id="gastos" className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
---
+++ app/balance/section-cards.tsx
@@ -0,0 +1,62 @@
+import { TrendingDown, TrendingUp } from "lucide-react"
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardFooter
+} from "@/common/components/ui/card"
+
+const DATA = [
+  {
+    id: 1,
+    title: "Debe",
+    amount: "$146.342,00",
+    trending: "up",
+    info: "bajale a las compras"
+  },
+  {
+    id: 2,
+    title: "Haber",
+    amount: "$177.945,00",
+    trending: "down",
+    info: "requiere mas atención"
+  },
+  {
+    id: 3,
+    title: "Resultado",
+    amount: "$316.027,87",
+    trending: "up",
+    info: "superando los objetivos"
+  }
+]
+
+export function SectionCards() {
+  return (
+    <div className="grid py-2 auto-rows-min gap-4 md:grid-cols-3">
+        {DATA.map((item,i) => (
+        <Card key={i} className="@container/card dark">
+        <CardHeader>
+          <CardDescription>{item.title}</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {item.amount}
+          </CardTitle>
+          <CardAction>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+            <div className="line-clamp-1 flex gap-2 font-medium">
+                {item.trending === "up" ? "subiendo los gastos" : "bajaste 20% este periodo"} {item.trending === "up" ? <TrendingUp className="size-4" /> : <TrendingDown className="size-4" />}
+            </div>
+          <div className="text-muted-foreground">
+            {item.info}
+          </div>
+        </CardFooter>
+      </Card>   
+        ))}
+    </div>
+  )
+}
+
